aff7b68e66f46147f74d4b11a6f3db5518c19526
Verena <vero_ds@web.de>
2021-10-11 17:04:07 +0200

    A01_RandomPoem

---
+++ A01_RandomPoem/RandomPoem.js
@@ -0,0 +1,23 @@
+"use strict";
+var RandomPoem;
+(function (RandomPoem) {
+    let subjects = ["Hannah", "Nesli", "Ronja", "Steffen", "Jasmin", "Lili"];
+    let predicates = ["verschüttet", "mag", "kauft", "trinkt", "klaut", "öffnet"];
+    let objects = ["die Cola", "das Bier", "das Red Bull", "den Wein", "das Wasser", "den Saft"];
+    for (let index = 5; index >= 1; index--) {
+        console.log(index);
+        let showPoem = getVerse(subjects, predicates, objects);
+        console.log(showPoem);
+    }
+    function getVerse(subjects, predicates, objects) {
+        let word1 = Math.floor(Math.random() * subjects.length);
+        let word2 = Math.floor(Math.random() * predicates.length);
+        let word3 = Math.floor(Math.random() * objects.length);
+        let words = subjects[word1] + " " + predicates[word2] + " " + objects[word3];
+        subjects.splice(word1, 1);
+        predicates.splice(word2, 1);
+        objects.splice(word3, 1);
+        return words;
+    }
+})(RandomPoem || (RandomPoem = {}));
+//# sourceMappingURL=RandomPoem.js.map
---
+++ A01_RandomPoem/RandomPoem.ts
@@ -0,0 +1,26 @@
+namespace RandomPoem {
+    let subjects: string[] = ["Hannah", "Nesli", "Ronja", "Steffen", "Jasmin", "Lili"];
+    let predicates: string[] = ["verschüttet", "mag", "kauft", "trinkt", "klaut", "öffnet"];
+    let objects: string[] = ["die Cola", "das Bier", "das Red Bull", "den Wein", "das Wasser", "den Saft"];
+     
+    
+    for (let index: number = 5; index >= 1; index--) {
+    console.log(index);
+
+    let showPoem: string = getVerse (subjects, predicates, objects);
+    console.log(showPoem);
+   }
+
+    function getVerse (  subjects: string[], predicates: string[], objects: string[]) {
+    let word1: number = Math.floor(Math.random() * subjects.length);
+    let word2: number = Math.floor(Math.random() * predicates.length);
+    let word3: number = Math.floor(Math.random() * objects.length);
+    let words: string = subjects[word1] + " " + predicates[word2] + " " + objects[word3];
+    subjects.splice(word1, 1);
+    predicates.splice(word2, 1);
+    objects.splice(word3, 1);
+    return words;
+    }
+}
+
+
