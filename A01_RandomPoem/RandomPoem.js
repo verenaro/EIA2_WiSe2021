56634ce3018baba6df18a3308a607cca50d792bc
Verena <vero_ds@web.de>
2021-10-13 16:59:02 +0200

    Verbesserung - Zahlen (console.log) weg

--- A01_RandomPoem/RandomPoem.js
+++ A01_RandomPoem/RandomPoem.js
@@ -1,14 +1,15 @@
 "use strict";
 var RandomPoem;
-(function (RandomPoem) {
+(function(RandomPoem) {
     let subjects = ["Hannah", "Nesli", "Ronja", "Steffen", "Jasmin", "Lili"];
     let predicates = ["verschüttet", "mag", "kauft", "trinkt", "klaut", "öffnet"];
     let objects = ["die Cola", "das Bier", "das Red Bull", "den Wein", "das Wasser", "den Saft"];
     for (let index = 5; index >= 1; index--) {
         console.log(index);
         let showPoem = getVerse(subjects, predicates, objects);
-        console.log(showPoem);
+
     }
+
     function getVerse(subjects, predicates, objects) {
         let word1 = Math.floor(Math.random() * subjects.length);
         let word2 = Math.floor(Math.random() * predicates.length);
