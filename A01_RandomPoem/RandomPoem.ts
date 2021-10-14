18eee0d0e2f8fe59a747dbe89d392c68078d76a9
Verena <vero_ds@web.de>
2021-10-14 12:39:43 +0200

    Cows_Übung

--- A01_RandomPoem/RandomPoem.ts
+++ A01_RandomPoem/RandomPoem.ts
@@ -2,24 +2,24 @@ namespace RandomPoem {
     let subjects: string[] = ["Hannah", "Nesli", "Ronja", "Steffen", "Jasmin", "Lili"];
     let predicates: string[] = ["verschüttet", "mag", "kauft", "trinkt", "klaut", "öffnet"];
     let objects: string[] = ["die Cola", "das Bier", "das Red Bull", "den Wein", "das Wasser", "den Saft"];
-     
-    
+
+
     for (let index: number = 5; index >= 1; index--) {
-    //console.log(index);
+        //console.log(index);
 
-    let showPoem: string = getVerse (subjects, predicates, objects);
-    console.log(showPoem);
-   }
+        let showPoem: string = getVerse(subjects, predicates, objects);
+        console.log(showPoem);
+    }
 
-    function getVerse (  _subjects: string[], _predicates: string[], _objects: string[]): string {
-    let word1: number = Math.floor(Math.random() * subjects.length);
-    let word2: number = Math.floor(Math.random() * predicates.length);
-    let word3: number = Math.floor(Math.random() * objects.length);
-    let words: string = subjects[word1] + " " + predicates[word2] + " " + objects[word3];
-    subjects.splice(word1, 1);
-    predicates.splice(word2, 1);
-    objects.splice(word3, 1);
-    return words;
+    function getVerse(_subjects: string[], _predicates: string[], _objects: string[]): string {
+        let word1: number = Math.floor(Math.random() * subjects.length);
+        let word2: number = Math.floor(Math.random() * predicates.length);
+        let word3: number = Math.floor(Math.random() * objects.length);
+        let words: string = subjects[word1] + " " + predicates[word2] + " " + objects[word3];
+        subjects.splice(word1, 1);
+        predicates.splice(word2, 1);
+        objects.splice(word3, 1);
+        return words;
     }
 }
 
